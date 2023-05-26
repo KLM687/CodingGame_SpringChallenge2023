68aa6cf2c44251d82b6c1bc63b4e79c950b836b4
Francesco LEE <flee@e1r6p9.clusters.42paris.fr>
2023-05-26 20:16:20 +0200

    NEED FIX

--- SpringChallenge2023/sources/compute.c
+++ SpringChallenge2023/sources/compute.c
@@ -6,13 +6,13 @@ void print_targets(t_target *targets, int number_of_bases){
 		{
 			for (int x = 0; x < TURN; x++)
 			{
-				if (targets[i].eggs[x].cell_index != 0 && MAX_EGGS > 0)
+				if (targets[i].eggs[x].cell_index >= 0 && MAX_EGGS > 0)
 					printf("LINE %d %d %d;", targets[i].base_index, targets[i].eggs[x].cell_index, 5);
 			}
 			if (TURN > 5)
 			{
 				for (int x = 0; x < TURN; x++){
-					if (targets[i].ressources[x].cell_index != 0 && MAX_RESSOURCES > 0)
+					if (targets[i].ressources[x].cell_index >= 0 && MAX_RESSOURCES > 0)
 						printf("LINE %d %d %d;", targets[i].base_index, targets[i].ressources[x].cell_index, 1);
 				}	
 			}
